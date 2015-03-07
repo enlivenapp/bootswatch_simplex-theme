120f297ea8cff2fb57f4f5bd955a131b4b5b02fe
Mike <sales@enlivenapp.com>
2015-03-07 18:19:11 -0500

    initial commit

---
+++ resources/lang/en/addon.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'name'        => 'Bootswatch Simplex',
+    'description' => 'A (Bootsrap) Bootswatch Simplex theme for PyroCMS v3.'
+];
---
+++ src/BootswatchSimplexTheme.php
@@ -0,0 +1,16 @@
+<?php namespace Enlivenapp\BootswatchSimplexTheme;
+
+use Anomaly\Streams\Platform\Addon\Theme\Theme;
+
+/**
+ * Class BootswatchSimplexTheme
+ *
+ * @link          http://enlivenapp.com
+ * @author        Enliven Applications <sales@enlivenapp.com>
+ * @author        Michael Webber <mw@enlivenapp.com>
+ * @package       Enlivenapp/bootstrapThreeTheme
+ */
+class BootswatchSimplexTheme extends Theme
+{
+
+}
